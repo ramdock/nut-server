bf0ec00ca978c4239f2793c5e2d17cc0521893d7
MARQUEZ Marcel <mmarquez@nexity.fr>
2019-09-01 18:26:18 +0200

    fix(entrypoint): fix nohup not working as docker use socket and not terminal to execute entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /root/nut
 
-nohup python3 nut.py -S &
+python3 nut.py -S >> nut.log
 
 #init scan of games folder and scan every 60 secs
 watch -n 60 wget -q --spider --user guest --password guest http://127.0.0.1:9000/api/scan
