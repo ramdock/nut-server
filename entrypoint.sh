577bd57b2696b62914744fabb1bc50f5fa9cc3dc
Marcel Marquez <marcel.marquez@outlook.com>
2021-03-29 14:21:45 +0200

    feat(cron): reduce frequency of cron task to check for new games (every minutes) + remove logs for this task

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 
 # add custom id and password to secure nut-server
 echo "\n$ID|$PASSWORD" >> /root/nut.src.latest/conf/users.conf
-echo "* * * * * wget --spider --user $ID --password $PASSWORD http://127.0.0.1:9000/api/scan >> /var/log/cron.log\n" > /etc/cron.d/scraping-nut
+
+#add crontask for regularly check for new games on /games folder
+echo "0-59 * * * * wget --spider --user $ID --password $PASSWORD http://127.0.0.1:9000/api/scan" > /etc/cron.d/scraping-nut
 chmod 0644 /etc/cron.d/scraping-nut && crontab /etc/cron.d/scraping-nut
 
 #cron -f &
 service cron start
 
 python3 /root/nut.src.latest/nut.py -S >> /var/log/nut.log
+
